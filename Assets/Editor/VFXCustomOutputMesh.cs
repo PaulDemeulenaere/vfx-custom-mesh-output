3353e67b0a6188f7200d3d191f2fb6737b21a46f
Paul Demeulenaere <pauld@unity3d.com>
2023-10-02 22:31:14 +0200

    Draft override

--- Assets/Editor/VFXCustomOutputMesh.cs
+++ Assets/Editor/VFXCustomOutputMesh.cs
@@ -8,5 +8,18 @@ namespace UnityEditor.VFX.URP
     class VFXCustomOutputMesh : VFXURPLitMeshOutput
     {
         public override string name => "Custom Output Mesh";
+
+        public override string codeGeneratorTemplate => "Assets/Editor/VFXCustomOutputMesh";
+
+        protected override IEnumerable<VFXPropertyWithValue> inputProperties
+        {
+            get
+            {
+                foreach (var inputProperty in base.inputProperties)
+                    yield return inputProperty;
+
+                yield return new VFXPropertyWithValue(new VFXProperty(typeof(float), "customDeformation"));
+            }
+        }
     }
 }
